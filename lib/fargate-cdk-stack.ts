18a5214da032e470be7ebd29425eff4053ca65db
jagathda <mail.jagath@gmail.com>
2024-09-08 11:48:34 +0300

    Created ecs task excution role

--- lib/fargate-cdk-stack.ts
+++ lib/fargate-cdk-stack.ts
@@ -1,7 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps } from "aws-cdk-lib";
 import { Vpc, SubnetType, SecurityGroup } from 'aws-cdk-lib/aws-ec2';
 import { Cluster } from 'aws-cdk-lib/aws-ecs';
-import { ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import { ManagedPolicy, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 export class FargateCdkStack extends cdk.Stack {
@@ -30,10 +31,18 @@ export class FargateCdkStack extends cdk.Stack {
     // Allow inbound traffic on port 80
     securityGroup.addIngressRule(cdk.aws_ec2.Peer.anyIpv4(), cdk.aws_ec2.Port.tcp(80), 'Allow HTTP traffic');
 
-    //Create an ECS Cluster in the VPC
+    // Create an ECS Cluster in the VPC
     const cluster = new Cluster(this, 'FargateCluster', {
       vpc,
       clusterName: 'FargateCluster',
+    });
+
+    // Create ECS task excution role
+    const excutionRole = new Role(this, 'EcsTaskExcutionRole', {
+      assumedBy: new ServicePrincipal('ecs-tasks.amazonaws.com'),
+      managedPolicies: [
+        ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazoneECSTaskExecutionRolePpolicy'),
+      ],
     });
 
   }
